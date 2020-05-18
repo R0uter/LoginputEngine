36c09cdd809b348ebd313299ed021c05595f669e
R0uter <lomyr.jo@gmail.com>
2020-05-18 22:39:59 +1000

    remove non-used sqlite option and lower weight when miss

--- dag/dag.py
+++ dag/dag.py
@@ -7,14 +7,12 @@ import math
 import res.pinyin_data
 
 kLMDB = 'LMDB'
-kSQLITE = 'SQLITE'
 kRAWDATA = 'RAWDATA'
 kGB18030 = 'gb18030'
 Database_Type = kLMDB
 
 LMDB_FILE = './result_files/transition.mdb'
 PY_DATABASE = './result_files/emission.db'
-SQLITE_DIR = './result_files/db.sqlite'
 GRAM1FILE = './result_files/1gram_transition.json'
 GRAM2FILE = './result_files/2gram_transition.json'
 GRAM3FILE = './result_files/3gram_transition.json'
@@ -47,19 +45,6 @@ if Database_Type == kLMDB:
     con.commit()
     print(env.stat(), env.info())
 
-if Database_Type == kSQLITE:
-    con = sqlite3.connect(SQLITE_DIR)
-    con.executescript('''
-        PRAGMA page_size = 4096;
-        PRAGMA locking_mode =  EXCLUSIVE;
-        PRAGMA temp_store = MEMORY;
-        PRAGMA cache_size = 40960;
-        PRAGMA mmap_size = 40960;
-        PRAGMA synchronous = OFF;
-        PRAGMA journal_mode = OFF;
-        PRAGMA query_only = 1;
-        ''')
-    con.commit()
 print('Done.')
 
 
@@ -95,15 +80,6 @@ def _get_gram_1_weight_from(word: str) -> float:
                 data = t.get('min_value'.encode(kGB18030))
         return struct.unpack('d', data)[0]
 
-    if Database_Type == kSQLITE:
-        c = con.execute(
-            'select weight from transition where w1="-" and w2="-" and w3 in (?,?)',
-            [word, 'min_value'])
-        w = -math.inf
-        for row in c:
-            w = max(row[0], w)
-        return w
-
 
 def _get_words_with_gram_1_weight_from(pinyin: [str]) -> {}:
     result = {}
@@ -118,9 +94,9 @@ def _get_words_with_gram_1_weight_from(pinyin: [str]) -> {}:
 def _get_gram_2_weight_from(last_one: str, one: str) -> float:
     if Database_Type == kRAWDATA:
         if one not in gram2data:
-            return gram1data['min_value']
+            return _get_gram_1_weight_from(one) * 0.7
         if last_one not in gram2data[one]:
-            return gram1data[one]
+            return _get_gram_1_weight_from(one) * 0.7
         return gram2data[one][last_one]
 
     if Database_Type == kLMDB:
@@ -131,24 +107,6 @@ def _get_gram_2_weight_from(last_one: str, one: str) -> float:
             return struct.unpack('d', data)[0]
         return _get_gram_1_weight_from(one)
 
-    if Database_Type == kSQLITE:
-        c = con.execute(
-            'select w2,w3,weight from transition where w1=? and w2 in (?,?) and w3 in (?,?)',
-            ['-', '-', last_one, one, 'min_value'])
-        w1, w2, w3 = (0., 0., 0.)
-        for row in c:
-            if row[0] == last_one:
-                w1 = row[2]
-                break
-            if row[1] == one:
-                w2 = row[2]
-                continue
-            if row[1] == 'min_value':
-                w3 = row[2]
-        if w1 != 0: return w1
-        if w2 != 0: return w2
-        return w3
-
 
 def _get_gram_3_weight_from(last_last_one: str, last_one: str,
                             one: str) -> float:
@@ -156,43 +114,19 @@ def _get_gram_3_weight_from(last_last_one: str, last_one: str,
         if one in gram3data:
             if last_one in gram3data[one]:
                 if last_last_one not in gram3data[one][last_one]:
-                    return _get_gram_2_weight_from(last_one, one)
+                    return _get_gram_2_weight_from(last_one, one) * 0.7
 
                 return gram3data[one][last_one][last_last_one]
 
-        return _get_gram_1_weight_from(one)
+        return _get_gram_2_weight_from(last_one, one) * 0.7
 
     if Database_Type == kLMDB:
         key = '{}_{}_{}'.format(last_last_one, last_one, one).encode(kGB18030)
-        # key2 = '{}_{}'.format(last_one, one).encode(kGB18030)
-        # key3 = one.encode(kGB18030)
         with env.begin() as t:
             data = t.get(key)
         if data:
             return struct.unpack('d', data)[0]
         return _get_gram_2_weight_from(last_one, one)
-
-    if Database_Type == kSQLITE:
-        c = con.execute(
-            'select w1,w2,w3,weight from transition where w1 in (?,?) and w2 in (?,?) and w3 in (?,?)',
-            ['-', last_last_one, '-', last_one, one, 'min_value'])
-        w1, w2, w3, w4 = (0., 0., 0., 0.)
-        for row in c:
-            if row[0] == last_last_one:
-                w1 = row[3]
-                break
-            if row[1] == last_one:
-                w2 = row[3]
-                continue
-            if row[2] == one:
-                w3 = row[3]
-                continue
-            if row[2] == 'min_value':
-                w4 = row[3]
-        if w1 != 0: return w1
-        if w2 != 0: return w2
-        if w3 != 0: return w3
-        return w4
 
 
 def get_candidates_from(py: str, path_num=6, log=False) -> list:
